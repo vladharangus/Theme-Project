5e23ef09cfa0b69cd774524d8c038b373c7a4bf0
vladharangus <vlad.hara99@gmail.com>
2021-01-04 09:38:32 +0200

    First commit

---
+++ Phobia/Assets/spawn.cs
@@ -0,0 +1,36 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : MonoBehaviour
+{
+    public GameObject Foodprefab;
+    public Vector3 center;
+    public Vector3 size;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Q))
+            SpawnFood();
+    }
+    public void  SpawnFood()
+    {
+        Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z / 2, size.z / 2));
+        var sp = Instantiate(Foodprefab, pos, Quaternion.identity) as GameObject;
+        sp.AddComponent<move1>();
+        sp.transform.Rotate(0, Random.Range(0, 360), 0, Space.World);
+
+
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawCube(center, size);
+    }
+}
