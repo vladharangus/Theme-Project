0aea59c4e174710aaa18f36770448d2303a56e24
jercanioana <ioana.jercan02@gmail.com>
2021-01-06 18:14:10 +0200

    spiders not falling

---
+++ Phobia/Assets/notCrossing.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class notCrossing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody rb;
+    public float force = 50;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.CompareTag("Boundary"))
+            rb.AddForce(col.contacts[0].normal * force, ForceMode.Impulse);
+    }
+
+
+}
--- Phobia/Assets/spawn.cs
+++ Phobia/Assets/spawn.cs
@@ -17,13 +17,16 @@ public class spawn : MonoBehaviour
     void Update()
     {
         if (Input.GetKey(KeyCode.Q))
-            SpawnFood();
+            SpawnSpiders();
     }
-    public void  SpawnFood()
+    public void  SpawnSpiders()
     {
         Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z / 2, size.z / 2));
         var sp = Instantiate(Foodprefab, pos, Quaternion.identity) as GameObject;
         sp.AddComponent<move1>();
+        sp.AddComponent<Rigidbody>();
+        sp.AddComponent<BoxCollider>();
+        sp.AddComponent<notCrossing>();
         sp.transform.Rotate(0, Random.Range(0, 360), 0, Space.World);
 
 
@@ -33,4 +36,5 @@ public class spawn : MonoBehaviour
         Gizmos.color = new Color(1, 0, 0, 0.5f);
         Gizmos.DrawCube(center, size);
     }
+
 }
