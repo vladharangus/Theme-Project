f8dc5e8522b02bc1a5d92c041ccce600138a1857
vladharangus <vlad.hara99@gmail.com>
2021-01-14 00:02:07 +0200

    Final

--- Phobia/Assets/move1.cs
+++ Phobia/Assets/move1.cs
@@ -5,17 +5,15 @@ using UnityEngine;
 public class move1: MonoBehaviour
 {
 
-    AudioSource source;
     // Start is called before the first frame update
     void Start()
     {
-       source =  GetComponent<AudioSource>();
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        source.Play();
         transform.Translate(Vector3.forward * Time.deltaTime * 5);
     }
     
--- Phobia/Assets/moveCamera.cs
+++ Phobia/Assets/moveCamera.cs
@@ -5,41 +5,40 @@ using UnityEngine;
 public class moveCamera : MonoBehaviour
 {
     int speed = 30;
-    AudioSource source;
     // Start is called before the first frame update
     void Start()
     {
-        source = GetComponent<AudioSource>();
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            source.Play();
-            transform.Translate(new Vector3(-3*speed * Time.deltaTime, 0, 0));
+            
+            transform.Translate(new Vector3(-5*speed * Time.deltaTime, 0, 0));
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
-            source.Play();
-            transform.Translate(new Vector3(3*speed * Time.deltaTime, 0, 0));
+            
+            transform.Translate(new Vector3(5*speed * Time.deltaTime, 0, 0));
         }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow))
         {
-            source.Play();
-            transform.Translate(new Vector3(0, 0,3*speed * Time.deltaTime));
+            
+            transform.Translate(new Vector3(0, 0,5*speed * Time.deltaTime));
         }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow))
         {
-            source.Play();
-            transform.Translate(new Vector3(0, 0, -3*speed * Time.deltaTime));
+            
+            transform.Translate(new Vector3(0, 0, -5*speed * Time.deltaTime));
 
         }
         if (Input.GetKey(KeyCode.A))
-            transform.Rotate(-Vector3.up * speed * Time.deltaTime);
+            transform.Rotate(-Vector3.up * speed *5 *  Time.deltaTime);
 
         if (Input.GetKey(KeyCode.D))
-            transform.Rotate(Vector3.up * speed * Time.deltaTime);
+            transform.Rotate(Vector3.up * speed  *5* Time.deltaTime);
     }
 }
